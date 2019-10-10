cc3b80f631632e48f8dfb1791ae34708aa44b841
Iram Fattah <iramfattah94@gmail.com>
2019-10-09 23:56:59 -0400

    add persistence helpers

---
+++ AnimeListStarter/Model/Anime.swift
@@ -0,0 +1,45 @@
+//
+//  Anime.swift
+//  AnimeListStarter
+//
+//  Created by C4Q on 10/9/19.
+//  Copyright © 2019 Iram Fattah. All rights reserved.
+//
+
+import Foundation
+struct AnimeResultsWrapper: Codable {
+    let results: [Anime]
+    
+    static func getAnimeFromData(data: Data) throws ->  [Anime] {
+        var animeArr = [Anime]()
+        do {
+            let info = try JSONDecoder().decode(AnimeResultsWrapper.self, from: data)
+            animeArr = info.results
+            
+        } catch {
+            print(error)
+        }
+        return animeArr
+    }
+    
+}
+
+struct Anime: Codable {
+    let id: Int
+    let imageUrl: String
+    let title: String
+    let synopsis: String
+    let episodes: Int?
+    let score: Double
+    
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "mal_id"
+        case imageUrl = "image_url"
+        case title
+        case synopsis
+        case episodes
+        case score
+    }
+
+}
