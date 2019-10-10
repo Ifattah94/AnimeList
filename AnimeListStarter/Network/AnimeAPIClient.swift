3de0f503d7144d3482f6b86fd9616647490da43d
Iram Fattah <iramfattah94@gmail.com>
2019-10-10 11:58:21 -0400

    uodate

---
+++ AnimeListStarter/Network/AnimeAPIClient.swift
@@ -0,0 +1,9 @@
+//
+//  AnimeAPIClient.swift
+//  AnimeListStarter
+//
+//  Created by C4Q on 10/10/19.
+//  Copyright © 2019 Iram Fattah. All rights reserved.
+//
+
+import Foundation
---
+++ AnimeListStarter/View/AnimeCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnimeCell.swift
+//  AnimeListStarter
+//
+//  Created by C4Q on 10/10/19.
+//  Copyright © 2019 Iram Fattah. All rights reserved.
+//
+
+import UIKit
+
+class AnimeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
